8cde84021810f0d43c94b4d2757a7f754caeb673
shaantanu9 <shantanubombatkar2@gmail.com>
2024-10-03 17:26:55 +0530

    ts config

--- add.ts
+++ add.ts
@@ -1,8 +1,11 @@
-const addNumberArray = (numbers:number[]) => {
-    return numbers.reduce((acc, crr) =>{
-        if(crr < 0) throw new Error("negatives not allowed")
-        return acc + crr
-    });
+const addNumberArray = (numbers: number[]) => {
+  const num = numbers.filter((n) => n < 0);
+  if (num.length > 0)
+    throw new Error(`negatives not allowed ${num.join(",")} `);
+  return numbers.reduce((acc, crr) => {
+    if (crr < 0) throw new Error("negatives not allowed");
+    return acc + crr;
+  });
 };
 
 const add = (str: string) => {
@@ -13,21 +16,17 @@ const add = (str: string) => {
   if (str.startsWith("//")) {
     const [delimiter, numbersString] = str.split("//")[1].split("\n");
     const numbers = numbersString.split(delimiter).map(Number);
-    console.log({delimiter,numbersString, numbers})
-    
-    return numbers.reduce((acc, crr) => acc + crr);
+    console.log({ delimiter, numbersString, numbers });
+
+    return addNumberArray(numbers);
   }
 
   if (str.includes(",") || str.includes("\n")) {
     const numbers = str.split(/,|\n/).map(Number);
-    console.log(numbers)
-    return addNumberArray(numbers)
-    // numbers.reduce((acc, crr) => acc + crr);
+    console.log(numbers);
+    return addNumberArray(numbers);
   }
-
-
-
 };
 
-const val = "\n1\n2,-13"
-console.log(add(val))
+const val = "\n1\n12,13";
+console.log(add(val));
