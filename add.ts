f7a0496f8da73e9616fa656820f99359e6bab17a
shaantanu9 <shantanubombatkar2@gmail.com>
2024-10-03 17:11:42 +0530

    added jest

--- add.ts
+++ add.ts
@@ -1,1 +1,18 @@
-console.log("first")
+const add = (str: string) => {
+
+    if(str=="") return 0
+
+    if(str.length==1) return 0
+
+    if(str.includes(",") || str.includes("\n") ){
+        const numbers = str.split(/,|\n/).map(Number)
+        return numbers.reduce((acc,crr)=>acc+crr)
+    }
+
+    if(str.startsWith("//")){
+        const [delimiter, numbersString] = str.split("//")[1].split("\n")
+        const numbers = numbersString.split(delimiter)
+        return numbers.reduce((acc,crr)=>acc+crr)
+    }
+
+};
