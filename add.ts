cca15c2a3ede237073409df7634e10a99b4f3ef9
shaantanu9 <shantanubombatkar2@gmail.com>
2024-10-03 17:19:01 +0530

    ts config

--- add.ts
+++ add.ts
@@ -1,18 +1,28 @@
+// const addNumberArray = (numberArray) => {
+//   console.log(numberArray);
+// };
+
 const add = (str: string) => {
+  if (str == "") return 0;
 
-    if(str=="") return 0
+  if (str.length == 1) return Number(str);
 
-    if(str.length==1) return 0
+  if (str.startsWith("//")) {
+    const [delimiter, numbersString] = str.split("//")[1].split("\n");
+    const numbers = numbersString.split(delimiter).map(Number);
+    console.log({delimiter,numbersString, numbers})
+    
+    return numbers.reduce((acc, crr) => acc + crr);
+  }
 
-    if(str.includes(",") || str.includes("\n") ){
-        const numbers = str.split(/,|\n/).map(Number)
-        return numbers.reduce((acc,crr)=>acc+crr)
-    }
-
-    if(str.startsWith("//")){
-        const [delimiter, numbersString] = str.split("//")[1].split("\n")
-        const numbers = numbersString.split(delimiter)
-        return numbers.reduce((acc,crr)=>acc+crr)
-    }
+  if (str.includes(",") || str.includes("\n")) {
+    const numbers = str.split(/,|\n/).map(Number);
+    return numbers.reduce((acc, crr) => acc + crr);
+  }
 
 };
+
+const val = "//;\n1;2;3"
+console.log(
+    add(val)
+)
